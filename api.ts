826aca92fec37beee133ced61bf00ba947cbbea4
Mika Lindelä <mika.lindela@kelalab.onmicrosoft.com>
2021-09-09 16:12:22 +0300

    initial connection stuff to ledger

--- api.ts
+++ api.ts
@@ -7,40 +7,40 @@ const __filename = fileURLToPath(import.meta.url);
 const dir = path.dirname(__filename);
 const apiRouter = new Express.Router();
 console.log(dir);
-apiRouter.get('/api/hello', (req,res,next) => {
+apiRouter.get('/api/hello', (req, res, next) => {
     console.log('call registered');
     res.status(200).send();
     next();
 });
 
-apiRouter.get('/connections', async(req,res,next) => {
+apiRouter.get('/connections', async (req, res, next) => {
     console.log('connections call registered');
-    const data:any = await readFile('mock/connections.json');
+    const data: any = await readFile('mock/connections.json');
     console.log(data);
     const obj = JSON.parse(data);
     res.json(obj);
 });
 
-apiRouter.get('/proofs', async(req,res,next) => {
+apiRouter.get('/proofs', async (req, res, next) => {
     console.log('proofs call registered');
-    const data:any = await readFile('mock/proofs.json');
+    const data: any = await readFile('mock/proofs.json');
     console.log(data);
     const obj = JSON.parse(data);
     res.json(obj);
 });
 
-const readFile = async(filePath) => {
+const readFile = async (filePath) => {
     return new Promise((resolve, reject) => {
-        fs.readFile(path.resolve(dir,filePath),'utf8', (err,data) => {
-            console.log('err:', err, 'data:',data);
-            if(err){
-    
-            }else{
+        fs.readFile(path.resolve(dir, filePath), 'utf8', (err, data) => {
+            console.log('err:', err, 'data:', data);
+            if (err) {
+
+            } else {
                 resolve(data);
             }
         });
     })
-  
+
 }
 
 export default apiRouter;
--- index.ts
+++ index.ts
@@ -5,8 +5,134 @@ const server = createServer(app);
 import apiRouter from './api.js';
 import { Server } from 'socket.io';
 import socket from './websocket.js';
+import fetch from 'node-fetch';
+import fs from 'fs';
+
 const port = 4000;
 const io = new Server(server);
+
+const agency_url = 'http://13.79.168.138:8080';
+
+const createWallet = async (wallet_name?:String) => {
+    try {
+        const response = await fetch(`${agency_url}/multitenancy/wallet`, {
+            method: 'POST',
+            body: JSON.stringify({
+                label: 'Testi_Anna',
+                wallet_dispatch_type: 'default',
+                wallet_key: 'OmaTestiAvain123',
+                wallet_name: wallet_name,
+                wallet_type: 'indy'
+            }),
+        });
+        return response;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
+
+const getWallets = async (wallet_name?: String) => {
+    console.log('---> getWallets');
+    //const response = await fetch(`${agency_url}/multitenancy/wallets?wallet_name=${wallet_name}`);
+    const response = await fetch(`${agency_url}/multitenancy/wallets${wallet_name? '?wallet_name='+wallet_name: ''}`);
+    const json = await response.json();
+    console.log('<--- getWallets', json);
+    return json;
+}
+
+const getWallet = async (id?: String) => {
+    const response = await fetch(`${agency_url}/multitenancy/wallet/${id}`);
+    const json = await response.json();
+    console.log('getWallet', json);
+    return json;
+}
+
+const createDid = async (token: String) => {
+    const response = await fetch(`${agency_url}`, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    });
+}
+
+const getStatus = async () => {
+    try {
+        const response = await fetch(`${agency_url}/status`);
+        return response;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
+
+interface TokenJSON {
+    token?: String,
+}
+
+const getToken = async (id) => {
+    try {
+        const response = await fetch(`${agency_url}/multitenancy/wallet/${id}/token`, {
+            method: 'POST',
+            body: JSON.stringify({
+
+            })
+        });
+        const json:TokenJSON = await response.json();
+        const token = json.token;
+        return token;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
+
+interface WalletResponse {
+    wallet_id: String,
+    created_at: String,
+    key_management_mode: String,
+    updated_at: String,
+    settings: {
+      'wallet.type': 'indy',
+      'wallet.name': 'Testi_Anna_Lompakko2',
+      'wallet.webhook_urls': [],
+      'wallet.dispatch_type': 'base',
+      default_label: 'Testi_Anna',
+      'wallet.id': '3251082c-8d05-4711-9dd4-d5fcebf3ee8d'
+    },
+    token: String
+}
+    
+
+getStatus().then((response:any) => {
+    console.log(response);
+    const wallet_name = 'Testi_Anna_Lompakko';
+    if (response.status === 200) {
+        console.log('connection ok');
+        createWallet(wallet_name).then(walletresponse => {
+            console.log(walletresponse);
+            if (walletresponse.status === 200) {
+                walletresponse.json().then((json:WalletResponse) => {
+                    console.log('json', json);
+                    const token:String = json.token;
+
+                });
+            } else {
+                openSesame(wallet_name);
+            }
+        })
+    }
+});
+
+const openSesame = async(wallet_name: String) => {
+    const wallets:any = await getWallets(wallet_name);
+    const mywallet = wallets.results[0];
+    console.log(mywallet);
+    console.log('wallet', mywallet);
+    await getWallet(mywallet.wallet_id);
+    const token = await getToken(mywallet.wallet_id);
+    console.log('token acquired: ', token);
+}
 
 /** init websocket stuff */
 socket(io);
